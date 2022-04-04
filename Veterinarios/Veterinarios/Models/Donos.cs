f32a11591d42b5ccd7b696f701673077c1116373
RubenPPP <aluno20966@ipt.pt>
2022-04-04 16:15:20 +0100

    V.0.1.1 - Runtime fix and Donos code addition

--- Veterinarios/Veterinarios/Models/Donos.cs
+++ Veterinarios/Veterinarios/Models/Donos.cs
@@ -16,16 +16,22 @@ namespace Veterinarios.Models
         /// Nome do Dono do animal
         /// </summary>
         [Required(ErrorMessage = "O {0} é de preenchimento obrigatório!")]
+        [StringLength(30, ErrorMessage = "O {0} não pode ter mais do que {1} caracteres.")]
+        [RegularExpression("[ÂÓA-Za-záéíóúàèìòùâêîôûãõäëïöüñç '-]+", ErrorMessage = "No {0} só são aceites letras.")]
         public string Nome { get; set; }
         /// <summary>
         /// NIF do dono do animal
         /// </summary>
         [Required(ErrorMessage = "O {0} é de preenchimento obrigatório!")]
+        [StringLength (9, MinimumLength = 9, ErrorMessage = "O {0} deve ter exatamente {1} caracteres.")]
+        [RegularExpression("[123578]+[0-9]{8}", ErrorMessage = "O {0} deve começar por 1, 2, 3, 4, 5, 7 ou 8, e só ter algarismos.")]
         public string NIF { get; set; }
         /// <summary>
         /// Sexo do dono 
         /// Ff - Feminino ; Mm - Masculino
         /// </summary>
+        [StringLength(1, ErrorMessage = "O {0} só aceita um carácter.")]
+        [RegularExpression("[FfMm]", ErrorMessage = "No {0} só se aceitam as letras F ou M.")]
         public string Sexo { get; set; }
         /// <summary>
         /// Lista dos animais do Dono
